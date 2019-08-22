836b2339df7dac0ca378705c74e6f45da25074b1
rx-pawan <pawan.mishra@rxlogix.com>
2019-08-22 18:58:27 +0530

    added nesting tag3

--- migration/4.2.0/scripts/testdb_1.sql
+++ migration/4.2.0/scripts/testdb_1.sql
@@ -1,3 +1,3 @@
 --liquibase formatted sql
 --changeset salerno:4.2.0_testdb
-insert into test2 (id, name) values (9, 'by jenkins rollback');
+insert into test2 (id, name) values (19, 'by jenkins rollback');
--- migration/4.2.0/scripts/testdb_2.sql
+++ migration/4.2.0/scripts/testdb_2.sql
@@ -1,3 +1,3 @@
 --liquibase formatted sql
 --changeset salerno:4.2.0_testdb2
-insert into test2 (id, name) values (9, 'by jenkins-rollba2');
+insert into test2 (id, name) values (20, 'by jenkins-rollba2');
